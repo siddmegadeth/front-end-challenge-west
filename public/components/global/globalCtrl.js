d2b88a5a4a9cb6950919a7bffc9d14c67b4b5a8e
siddmegadeth <siddmegadeth@gmail.com>
2018-12-12 04:24:18 +0530

    Added JSON Data

--- public/components/global/globalCtrl.js
+++ public/components/global/globalCtrl.js
@@ -1,4 +1,8 @@
-app.controller('globalCtrl', ['$scope', '$rootScope',function($scope, $rootScope) {
+app.controller('globalCtrl', ['$scope', '$rootScope', 'product','$location', function($scope, $rootScope, product,$location) {
 
-       
+    $rootScope.isActive = function(loc)
+    {
+    	return $location.path() === loc;
+    }
+
 }]);
