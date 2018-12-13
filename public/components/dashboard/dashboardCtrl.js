4d427047dd6ed966d7670af252687a433baec759
siddmegadeth <siddmegadeth@gmail.com>
2018-12-14 05:17:25 +0530

    Fixed UI Issues And Added Responsive using Media for SMaller Devices

--- public/components/dashboard/dashboardCtrl.js
+++ public/components/dashboard/dashboardCtrl.js
@@ -1,19 +1,49 @@
-app.controller('dashboardCtrl', ['$scope', 'productService', '$location', function($scope, productService, $location) {
+app.controller('dashboardCtrl', ['$scope', 'productService', '$location', '$uibModal', function($scope, productService, $location, $uibModal) {
 
     var index = 0;
     productService.getList(function(resp) {
         $scope.productList = resp;
-       
+
     }, function(err) {
         error(err);
     });
 
     $scope.gotoProductDetails = function(index, product) {
-        log(index);
-        log(product);
 
         productService.setProductId(product);
-        $location.path("product-detail");
+        $uibModal.open({
+                templateUrl: "components/dashboard/templates/product-detail.html",
+                controller: ['productService', '$scope', function(productService, $scope) {
+
+                    $scope.rate = 2;
+                    $scope.max = 5;
+
+                    $scope.myInterval = 3000;
+                    $scope.noWrapSlides = false;
+                    $scope.activeSlide = 0;
+                    $scope.productDetail = productService.getProductId(product);
+                    log($scope.productDetail);
+                }]
+            })
+            .closed.then(function() {
+
+            });
     }
 
 }]);
+
+// $scope.productId = productService.getProductId();
+
+//     if (!$scope.productId) {
+//         $uibModal.open({
+//                 template: "<div><h1 class='text-center'>You Have Not Selected Any Product</h1><h5 class='text-center'><a href>Redirecting To Dashboard</a></h5></div>",
+//             })
+//             .closed.then(function() {
+//                 $location.path('dashboard');
+//             });
+
+//     } else {
+//         log($scope.productId)
+
+
+//     }
